c5176132ea2f3c7b2bb409eb29a7fe1b0bfbc797
David Harting <david.e.harting@gmail.com>
2022-06-11 16:23:51 -0400

    fetch library index

--- src/clients/little-free-library.ts
+++ src/clients/little-free-library.ts
@@ -1,11 +1,46 @@
 import axios from 'axios';
+import { get, put } from '../cache';
+import { z } from 'zod';
+import { logger } from '../logger';
 
 // https://appapi.littlefreelibrary.org/library/pin.json?page_size=50&distance=50&near=indiana
 
-export const getLibraryIndex = async () => {
+export const getLibraryIndex = async (): Promise<LibraryIndexPayload> => {
+  const CACHE_KEY = 'all-libraries.json';
+
+  const contents = await get(CACHE_KEY);
+  if (contents !== null) {
+    logger.trace('Returning cached contents.');
+    return zLibraryIndex.passthrough().parse(JSON.parse(contents));
+  }
+
+  logger.trace({}, 'Cache missed. Attempting API call for libraries');
   const response = await axios.get(
     'https://appapi.littlefreelibrary.org/library/pin.json',
     { params: { page_size: 500, distance: 50, near: 'indiana' } }
   );
-  console.log(JSON.stringify(response));
+
+  const json = response.data;
+  const payload = zLibraryIndex.passthrough().parse(json);
+  await put('all-libraries.json', JSON.stringify(payload));
+  return payload;
 };
+
+const zLibraryIndex = z.object({
+  page: z.number().nonnegative(),
+  page_size: z.number().positive(),
+  page_count: z.number().nonnegative(),
+  library_count: z.number().nonnegative(),
+  distance: z.number().nonnegative(),
+  latitude: z.number().optional(),
+  longitude: z.number().optional(),
+  libraries: z.array(
+    z.object({
+      id: z.number().positive(),
+      Library_Geolocation__Latitude__s: z.number().optional(),
+      Library_Geolocation__Longitude__s: z.number().optional(),
+    })
+  ),
+});
+
+type LibraryIndexPayload = z.infer<typeof zLibraryIndex>;
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { getLibraryIndex } from './clients/little-free-library';
 
 async function main() {
+  console.time('main');
   await getLibraryIndex();
+  console.timeEnd('main');
 }
 
 main();
