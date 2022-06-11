3f6c08ad641f9fa5057e35c662009275543435d6
David Harting <david.e.harting@gmail.com>
2022-06-11 16:09:28 -0400

    add support for getting file

--- src/cache/index.test.ts
+++ src/cache/index.test.ts
@@ -1,15 +1,27 @@
-import { put, destroyCache } from './index';
+import { get, put, destroyCache } from './index';
 import { logger } from '../logger';
 
 describe('cache', () => {
+  afterEach(async done => {
+    const result = await destroyCache();
+    logger.info({ result }, 'Destroyed directory from afterEach');
+    done();
+  });
+
   describe('put', () => {
-    afterEach(async done => {
-      const result = await destroyCache();
-      logger.info({ result }, 'Destroyed directory from afterEach');
-      done();
+    it('should write contents to a file in .cache and they should be retrievable with get', async () => {
+      const key = 'abc-123.json';
+      const contents = '3423434 hello there';
+      await put(key, contents);
+      const retrievedContents = await get(key);
+      expect(retrievedContents).toBe(contents);
     });
-    it('should write contents to a file in .cache', async () => {
-      await put('abc-123.json', '3423434 hello there');
+  });
+
+  describe('get', () => {
+    it('should return null if the cache file does not exist', async () => {
+      const contents = await get('some-random-key/that-is-not-there.json');
+      expect(contents).toBe(null);
     });
   });
 });
--- src/cache/index.ts
+++ src/cache/index.ts
@@ -2,6 +2,9 @@ import fs from 'fs/promises';
 import path from 'path';
 import { logger } from '../logger';
 
+const cacheDirPath = (): string => path.join(process.cwd(), '.cache');
+const cacheFilePath = (key: string): string => path.join(cacheDirPath(), key);
+
 export async function put(key: string, value: string): Promise<void> {
   try {
     await fs.access('./.cache');
@@ -10,6 +13,17 @@ export async function put(key: string, value: string): Promise<void> {
     await fs.mkdir('./.cache');
   }
   await fs.writeFile(`./.cache/${key}`, value, { flag: 'w' });
+}
+
+export async function get(key: string): Promise<string | null> {
+  try {
+    const filePath = cacheFilePath(key);
+    await fs.access(filePath);
+    const contents = await fs.readFile(filePath);
+    return contents.toString();
+  } catch {
+    return null;
+  }
 }
 
 export async function destroyCache(): Promise<boolean> {
