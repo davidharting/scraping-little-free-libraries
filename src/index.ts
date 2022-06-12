27d0e03cb0bd654e5a04dd2fdf13b9af7d9ed314
David Harting <david.e.harting@gmail.com>
2022-06-12 16:37:19 -0400

    run through with more library

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,28 @@
+import Papa from 'papaparse';
 import {
   Library,
   getLibraryIndex,
   showLibrary,
 } from './clients/little-free-library';
+import { put } from './cache';
 import { logger } from './logger';
 
+const limit = (): number => {
+  const fallback = 10;
+  if (process.env.LIMIT) {
+    const parsed = parseInt(process.env.LIMIT);
+    if (parsed === NaN) {
+      return fallback;
+    }
+    return parsed;
+  }
+  return fallback;
+};
+
+const LIMIT = limit();
+
 async function main() {
+  logger.trace({ limit: LIMIT }, 'Kicking off a run.');
   const librariesIndex = await getLibraryIndex();
   logger.trace(
     { numLibraries: librariesIndex.libraries.length },
@@ -14,14 +31,43 @@ async function main() {
 
   const libraries: Array<Library> = [];
 
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < LIMIT; i++) {
     const item = librariesIndex.libraries[i];
     const library = await showLibrary({
       id: item.id,
-      delayInMsIfCacheMiss: 1500,
+      delayInMsIfCacheMiss: 1000,
     });
     libraries.push(library);
   }
+
+  logger.trace(
+    { numberOfLibraries: libraries.length },
+    'Finished aggregating libraries.'
+  );
+
+  const rows: Array<LibraryCsvRow> = libraries.map(library => {
+    const row: LibraryCsvRow = {
+      'Name of library': library.List_As_Name__c,
+      "Person's name": library.Primary_Steward_s_Name__c ?? null,
+      "Person's email": library.Primary_Steward_s_Email__c ?? null,
+      'Street address': library.Street__c ?? null,
+      City: library.City__c ?? null,
+      Zipcode: library.Postal_Zip_Code__c ?? null,
+      State: library.State_Province_Region__c ?? null,
+    };
+    return row;
+  });
+
+  const indianaRows = rows.filter(row => row.State === 'IN');
+  logger.trace(
+    { numberOfIndianaLibraries: indianaRows.length },
+    'Filtered to libraries in Indiana.'
+  );
+
+  const csv = Papa.unparse(indianaRows);
+  logger.trace('Prepared csv');
+  await put('output.csv', csv);
+  logger.trace('Done!');
 }
 
 (async () => {
@@ -29,3 +75,13 @@ async function main() {
   await main();
   console.timeEnd('main');
 })();
+
+interface LibraryCsvRow {
+  'Name of library': string | null;
+  "Person's name": string | null;
+  "Person's email": string | null;
+  'Street address': string | null;
+  City: string | null;
+  Zipcode: string | null;
+  State: string | null;
+}
